b6508f4fd7eef81413615aa623545588545e343f
n13i <n13i@m2hq.net>
2008-04-21 16:46:10 +0000

    * bombcloud のファイル名の ! を . に変更。

--- bombcloud.pl
+++ bombcloud.pl
@@ -42,7 +42,7 @@ while(my $update = $sth->fetchrow_hashref)
     #$target_enc = encode_base64(encode('utf8', $target));
     $target_enc = md5_base64(encode('utf8', $target));
     $target_enc =~ s/\n//g;
-    $target_enc =~ tr/\+\/=/!_\-/;
+    $target_enc =~ tr/\+\/=/\._\-/;
 
 	#print $count . "\t\t" . $target . "\n";
     my $filename = $target_enc . '.php';
